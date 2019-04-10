f7eba9d834590a2ffb6fdeafe81b1fcd4b160e6a
Lilia Albano <lilia.albano8198@gmail.com>
2019-04-10 15:51:06 -0400

    Initial commit

---
+++ boredom_quiz.py
@@ -0,0 +1,71 @@
+#boredom_quiz.py
+#this interactive 'boredom' quiz will help someone figure out what to do whrn they are bored
+
+#create the graphics window
+
+
+#create title screen graphics
+
+
+#ask first question "how far are you willing to travel?"
+
+
+#layout answers in order of "10 ft, 1 mile, 10 miles, out of the area"
+
+
+##if 10 ft is chosen then ask which would you rather do
+    #show choices of WATCH A MOVIE, READ A BOOK, DRAW/PAINT, and WRITE
+    
+    #if watch a show or movie is chosen then ask what genre do you prefer
+    #show choices of HORROR ROMANCE COMEDY and ACTION
+    
+        #if HORROR is chosen then ask if they pprefer newer or older productions
+        
+            #if NEWER is chosen then show the recommendations, where to watch it, and how much it would be for it to be chosen
+            
+            #if OLDER is chosen then show the recommendations, where to watch it, and how much it would be for it to be chosen
+        
+        #if ROMANCE is chosen then show the recommendation, where to watch it, and how much it would be for it to be chosen
+        
+        #if COMEDY is chosen then show the recommendation, where to watch it, and how much it would be for it to be chosen
+        
+        #if ACTION is chosen then show the recommendation, where to watch it, and how much it would be for it to be chosen
+        
+    
+    
+    ##if read a book is chosen then ask what kind of books do they like to read###
+    #show choices of HORROR, ROMANCE, COMEDY, and ACTION
+        #if HORROR is chosen then show the recommendation, where to read it, and how much it would be for it to be chosen
+        
+        #if ROMANCE is chosen then show the recommendation, where to read it, and how much it would be for it to be chosen
+        
+        #if COMEDY is chosen then show the recommendation, where to read it, and how much it would be for it to be chosen
+        
+        #if ACTION is chosen then show the recommendation, where to read it, and how much it would be for it to be chosen
+        
+    
+    
+    ##if draw/paint is chosen then ask what their preferred drawing style is
+        
+        #if LINE ART is chosen ask what theme appeals to them
+        #show choices of NATURE, WILD ANIMALS, EMOTIONS, AND BUILDINGS
+            
+        
+        #if RANDOM SKETCHES is chosen ask what theme appeals to them
+        
+        
+        #if WATER COLOR is chosen ask what theme appeals to them
+        
+        
+        #if ACRYLLIC PAINT is chosen ask what theme appeals to them
+
+
+
+
+#if 1 mile is chosen then show these answers
+
+
+#if 10 miles is chosen then show these answers
+
+
+#if out of the area is chosen then show these answers
